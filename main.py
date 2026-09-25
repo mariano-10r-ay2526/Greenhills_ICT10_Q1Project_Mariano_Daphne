24320304f37f7a4a54902601dc48dfbc29a86f5b
mariano-10r-ay2526 <mariano.dl@obmontessori.edu.ph>
2026-09-25 15:19:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 from pyscript import display, document
 
 # Creating the receipt
-def create_order(e):
-    document.getElementById("result").innerHTML = ""
 
+def create_order(e):
+    document.getElementById("result").innerHTML = " "
     prod1 = document.getElementById("chicken")
     prod2 = document.getElementById("sidedish")
     prod3 = document.getElementById("soda")
 
-    subtotal = (float(prod1.value) * prod1.checked) + \
-               (float(prod2.value) * prod2.checked) + \
-               (float(prod3.value) * prod3.checked)
+    subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked
+    display(f'The subtotal is ₱{subtotal:.2f}', target="result")
 
     vat_number = subtotal * 0.12
+    display(f'The VAT is ₱{vat_number:.2f}', target="result")
+
     total_amount = subtotal + vat_number
+    display(f'The total amount is ₱{total_amount:.2f}', target="result")
 
-    text = (
-        f"Your subtotal is ₱{subtotal:.2f}\n"
-        f"Your VAT is ₱{vat_number:.2f}\n"
-        f"Your total amount is ₱{total_amount:.2f}"
-    )
+# Creating the SKU generator
 
-    display(text, target="result")
-
-# Creating SKU
 def generate_sku(e):
 
     category = document.getElementById("category")
     product = document.getElementById("product")
     stock = document.getElementById("stock")
-    result_div = document.getElementById("result")
-
-    if not stock.value:
-        result_div.textContent = "Please enter a stock quantity."
-        return
 
     category_code = category.value
     product_name = product.value
     stock_quantity = stock.value
 
-    product_code = product_name.upper().replace(" ", "")[:4]
+    product_code = product_name.upper()
+    product_code = product_code.replace(" ", "")
+    product_code = product_code[:4]
 
-    sku = f"{category_code}-{product_code}-{stock_quantity}"
-    result_div.textContent = sku
+    sku = category_code + "-" + product_code + "-" + stock_quantity
+
+    document.getElementById("result").textContent = sku
